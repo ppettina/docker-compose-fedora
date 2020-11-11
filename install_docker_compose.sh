0a9613d3dafb520cc3aa1035b8344289354a4558
Paolo Pettinato <ppettina@cisco.com>
2020-11-11 10:15:09 +0000

    Add install script

---
+++ install_docker_compose.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+TEMPDIR="$(mktemp -d)"
+cleanup() {
+    rm -rf "$TEMPDIR"
+    docker rmi -f docker-compose-fedora || true
+}
+
+cd "$TEMPDIR"
+git clone https://github.com/ppettina/docker-compose-fedora.git
+cd docker-compose-fedora
+docker build . -t docker-compose-fedora
+docker run --rm -v /usr/local/bin:/dist docker-compose-fedora
